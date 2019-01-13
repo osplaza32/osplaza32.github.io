1de88a117eadacd1edf8c422ac1f63ac25be6c7b
Oscar Plaza <oscarent2@gmail.com>
2019-01-13 16:15:19 -0300

    add js main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log("Hola");
+// import i18next from 'i18next';
+
+i18next.init({
+    lng: 'en',
+    debug: true,
+    resources: {
+        en: {
+            translation: {
+                "key": "hello world"
+            }
+        },
+        es: {
+            translation: {
+                "key": "Hola mundo"
+            }
+        }
+    }
+}, function(err, t) {
+    // init set content
+    updateContent();
+});
+
+function updateContent() {
+    document.getElementById('output').innerHTML = i18next.t('key');
+}
+
+function changeLng(lng) {
+    i18next.changeLanguage(lng);
+}
+
+i18next.on('languageChanged', () => {
+    updateContent();
+});
